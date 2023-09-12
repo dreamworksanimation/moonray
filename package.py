8e6322f07e00c21772ad0329b619658b13ca31f7
jmahovsky <jeff.mahovsky@dreamworks.com>
2023-09-12 13:59:41 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.13'
+    _version = '14.14'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -69,7 +69,7 @@ requires = [
     'boost',
     'cuda-12.1.0.x',
     'embree-4.1.0.x',
-    'mcrt_denoise-3.13',
+    'mcrt_denoise-3.14',
     'mkl',
     'openexr',
     'openimageio-2.3.20.0.x',
@@ -77,7 +77,7 @@ requires = [
     'openvdb',
     'optix-7.3.0.x',
     'random123-1.08.3',
-    'scene_rdl2-12.11',
+    'scene_rdl2-12.12',
 ]
 
 private_build_requires = [
