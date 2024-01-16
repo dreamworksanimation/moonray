14ef2aee2ab4de6995ed66ac0f57233e418bcddd
scottc <scott.cegielski@dreamworks.com>
2024-01-16 13:51:25 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '15.9'
+    _version = '15.10'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -75,7 +75,7 @@ requires = [
     'boost',
     'cuda-12.1.0.x',
     'embree-4.2.0.x',
-    'mcrt_denoise-4.5',
+    'mcrt_denoise-4.6',
     'mkl',
     'openexr',
     'openimageio-2.3.20.0.x',
@@ -83,7 +83,7 @@ requires = [
     'openvdb',
     'optix-7.6.0.x',
     'random123-1.08.3',
-    'scene_rdl2-13.4',
+    'scene_rdl2-13.5',
 ]
 
 private_build_requires = [
