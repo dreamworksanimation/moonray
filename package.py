a741a28e6ae5c6bc0d754c356aba970dcd522ac8
rpacker <randy.packer@dreamworks.com>
2024-06-04 20:56:55 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '16.8'
+    _version = '16.9'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -53,7 +53,7 @@ requires = [
     'boost',
     'cuda-12.1.0.x',
     'embree-4.2.0.x',
-    'mcrt_denoise-5.6',
+    'mcrt_denoise-5.7',
     'mkl',
     'openexr',
     'openimageio-2.3.20.0.x',
@@ -61,7 +61,7 @@ requires = [
     'openvdb',
     'optix-7.6.0.x',
     'random123-1.08.3',
-    'scene_rdl2-14.6',
+    'scene_rdl2-14.7',
 ]
 
 private_build_requires = [
