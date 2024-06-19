71e3a7287d7ccfa26f732c5f75b1fdbc80b1047d
Mike Day <mike.day@dreamworks.com>
2024-06-18 21:02:19 -0700

    Comment out clang-17 debug build

--- package.py
+++ package.py
@@ -44,7 +44,7 @@ variants = [
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.0', 'gcc-11.x', 'amorphous-9', 'openvdb-10', 'imath-3', 'zlib-1.2.11.x'],
     ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'gcc-11.x', 'amorphous-9', 'openvdb-10', 'imath-3', 'zlib-1.2.11.x'],
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.0', 'clang-17.0.6.x', 'amorphous-9', 'openvdb-10', 'imath-3', 'zlib-1.2.11.x'],
-    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'clang-17.0.6.x', 'amorphous-9', 'openvdb-10', 'imath-3', 'zlib-1.2.11.x'],
+#    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'clang-17.0.6.x', 'amorphous-9', 'openvdb-10', 'imath-3', 'zlib-1.2.11.x'],
 ]
 
 conf_rats_variants = variants[0:2]
