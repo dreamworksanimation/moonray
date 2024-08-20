ef5781aed7ceb48d3df35a7951d0712a4261fdce
jlanz <Jon.Lanz@dreamworks.com>
2024-08-20 11:03:07 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '16.18'
+    _version = '16.19'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
