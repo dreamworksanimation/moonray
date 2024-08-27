b2bf5fe98ed2e32d5075ba93e079b2f418ac7f8f
alee1 <ashley.lee@dreamworks.com>
2024-08-27 14:25:33 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '16.19'
+    _version = '16.20'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -55,7 +55,7 @@ requires = [
     'boost',
     'cuda-12.1.0.x',
     'embree-4.2.0.x',
-    'mcrt_denoise-5.12',
+    'mcrt_denoise-5.13',
     'mkl',
     'openexr',
     'openimageio-2.3.20.0.x',
@@ -63,7 +63,7 @@ requires = [
     'openvdb',
     'optix-7.6.0.x',
     'random123-1.08.3',
-    'scene_rdl2-14.12',
+    'scene_rdl2-14.13',
 ]
 
 private_build_requires = [
